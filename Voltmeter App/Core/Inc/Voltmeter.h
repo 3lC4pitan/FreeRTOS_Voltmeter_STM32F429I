710be90513c084cf428a477348de053bc3e5d1c7
Amine NAOUR <15603323+3lC4pitan@users.noreply.github.com>
2021-07-15 03:28:35 +0300

    Add files via upload

---
+++ Voltmeter App/Core/Inc/Voltmeter.h
@@ -0,0 +1,18 @@
+/*
+ * Voltmeter.h
+ *
+ *  Created on: 3 Jan 2021
+ *      Author: nmeda
+ */
+
+#ifndef APPLICATION_USER_VOLTMETER_H_
+#define APPLICATION_USER_VOLTMETER_H_
+
+
+
+void VoltmeterInit();
+void Voltmeter();
+
+
+#endif /* APPLICATION_USER_VOLTMETER_H_ */
+
